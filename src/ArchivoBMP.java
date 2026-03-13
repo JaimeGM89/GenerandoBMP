c1ca83c45aa4f996f222bccddcdcaf33c94868fa
Jaime González Morete <jaimegonzalezmorete@gmail.com>
2026-03-13 13:06:27 +0100

    Clase creada

---
+++ src/ArchivoBMP.java
@@ -0,0 +1,12 @@
+
+public class ArchivoBMP {
+
+	public static void main(String[] args) {
+		// TODO Nombre de la imagen
+		// TODO Tamaño de la imagen
+		// TODO Tamaño de la figura
+		// TODO Color del cuadrado y fondo
+		
+		// TODO POSIBLES MEJORAS: Dibujar un círculo (0,50). Dibujar cuadrado sobre bmp (0,50)
+	}
+}

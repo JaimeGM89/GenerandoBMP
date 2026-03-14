2b001496dbcd34b74c63788cc21bfd5a9ea0039a
Jaime González Morete <jaimegonzalezmorete@gmail.com>
2026-03-14 17:51:58 +0100

    Programa funcionando sin extras

--- src/ArchivoBMP.java
+++ src/ArchivoBMP.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class ArchivoBMP {
@@ -21,9 +24,9 @@ public class ArchivoBMP {
 		Scanner scn = new Scanner(System.in);
 
 		String nombreImagen = "";
-		int tamanioImagen = -1, tamanioFigura = -1, rojoImagen, verdeImagen, azulImagen, rojoFigura, verdeFigura, azulFigura, limiteInferior,
-				limiteSuperior;
-		boolean esPar = false;
+		int tamanioImagen = -1, tamanioFigura = -1, rojoImagen, verdeImagen, azulImagen, rojoFigura, verdeFigura,
+				azulFigura, limiteInferior, limiteSuperior, tamanioCirculo;
+		boolean esPar = false, dibujarCirculo;
 
 		System.out.print("¿Qué nombre quieres darle a la imagen?: ");
 		nombreImagen = scn.nextLine().trim();
@@ -45,38 +48,102 @@ public class ArchivoBMP {
 				tamanioFigura = scn.nextInt();
 			else
 				System.out.println("Error: Dato no válido.");
-			
-			if(tamanioFigura > tamanioImagen)
+
+			if (tamanioFigura > tamanioImagen)
 				System.out.println("El tamaño debe ser inferior al de la imagen.");
 			scn.nextLine();
 		}
 
-		System.out.println("\nCOLORES DE LA IMAGEN");		
-		rojoImagen = solicitarInfo(scn, "Rojo(valor 0-255): ");
-		verdeImagen = solicitarInfo(scn, "Verde(valor 0-255): ");
-		azulImagen = solicitarInfo(scn, "Azul(valor 0-255): ");
+		System.out.println("\nCOLORES DE LA IMAGEN");
+		rojoImagen = solicitarInfo(scn, "Rojo (valor 0-255): ");
+		verdeImagen = solicitarInfo(scn, "Verde (valor 0-255): ");
+		azulImagen = solicitarInfo(scn, "Azul (valor 0-255): ");
 
 		System.out.println("\nCOLORES DE LA FIGURA");
-		rojoFigura = solicitarInfo(scn, "Rojo(valor 0-255): ");
-		verdeFigura = solicitarInfo(scn, "Verde(valor 0-255): ");
-		azulFigura = solicitarInfo(scn, "Azul(valor 0-255): ");
+		rojoFigura = solicitarInfo(scn, "Rojo (valor 0-255): ");
+		verdeFigura = solicitarInfo(scn, "Verde (valor 0-255): ");
+		azulFigura = solicitarInfo(scn, "Azul (valor 0-255): ");
 
 		limiteInferior = (tamanioImagen - tamanioFigura) / 2;
 		limiteSuperior = limiteInferior + tamanioFigura - 1;
 
 		File imagen = new File(nombreImagen + ".bmp");
+		RandomAccessFile rafImagen = null;
 
-		for (int i = 0; i < tamanioImagen; i++) {
-			for (int j = 0; j < tamanioImagen; j++) {
-				if ((i == limiteInferior || i == limiteSuperior) && j >= limiteInferior && j <= limiteSuperior) {
+		int bytesPorFila = tamanioImagen * 3;
+		int padding = (4 - (bytesPorFila % 4)) % 4;
 
-				} else if ((j == limiteInferior || j == limiteSuperior) && i >= limiteInferior && i <= limiteSuperior) {
+		try {
+			rafImagen = new RandomAccessFile(imagen, "rw");
+			añadirCabecera(rafImagen, nombreImagen, tamanioImagen, bytesPorFila, padding);
 
+			for (int i = 0; i < tamanioImagen; i++) {
+				for (int j = 0; j < tamanioImagen; j++) {
+
+					if ((i == limiteInferior || i == limiteSuperior) && j >= limiteInferior && j <= limiteSuperior) {
+						rafImagen.write(azulFigura);
+						rafImagen.write(verdeFigura);
+						rafImagen.write(rojoFigura);
+					} else if ((j == limiteInferior || j == limiteSuperior) && i >= limiteInferior
+							&& i <= limiteSuperior) {
+						rafImagen.write(azulFigura);
+						rafImagen.write(verdeFigura);
+						rafImagen.write(rojoFigura);
+					} else {
+						rafImagen.write(azulImagen);
+						rafImagen.write(verdeImagen);
+						rafImagen.write(rojoImagen);
+					}
+				}
+				for (int p = 0; p < padding; p++) {
+					rafImagen.write(0);
 				}
 			}
+
+			System.out.println("La imagen ha sido creada.");
+			rafImagen.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		// TODO POSIBLES MEJORAS: Dibujar un círculo (0,50). Dibujar cuadrado sobre bmp
 		// (0,50)
 
+	}
+
+	private static void añadirCabecera(RandomAccessFile rafImagen, String nombreArchivo, int tamanioImagen,
+			int bytesPorFila, int padding) throws FileNotFoundException, IOException {
+
+		int tamanioArchivo;
+		byte cabecera[];
+
+		cabecera = new byte[54];
+
+		// 3. Bytes totales de una fila
+		int filaTotal = bytesPorFila + padding;
+		tamanioArchivo = (14 + 40 + filaTotal * tamanioImagen);
+
+		cabecera[0] = 66;// Letra B
+		cabecera[1] = 77;// Letra M
+		tamanio(cabecera, tamanioArchivo, 2);
+		cabecera[10] = 54;
+		cabecera[14] = 40; // Tamaño del header inferior (fijo en este caso)
+		tamanio(cabecera, tamanioImagen, 18);
+		tamanio(cabecera, tamanioImagen, 22);
+		cabecera[26] = 1;
+		cabecera[28] = 24;
+		cabecera[46] = 2; // Numero de colores en la paleta
+		cabecera[50] = 2; // Numero de colores usados
+
+		rafImagen.write(cabecera);
+	}
+
+	public static void tamanio(byte[] array, int tamanio, int posicion) {
+		array[posicion] = (byte) (tamanio & 0xFF);
+		array[posicion + 1] = (byte) ((tamanio >> 8) & 0xFF);
+		array[posicion + 2] = (byte) ((tamanio >> 16) & 0xFF);
+		array[posicion + 3] = (byte) ((tamanio >> 24) & 0xFF);
 	}
 }
